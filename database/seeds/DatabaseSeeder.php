5916b9c1a972f679e4376d3fa47103f70ed6fd48
ShadyElDawy <shadyeldawy96@gmail.com>
2020-12-24 23:59:51 +0200

    seeding to db test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory( \App\User::class, 50 ) -> create();  //Every one of 50 will use /factories/UserFactory.php
     }
 }
